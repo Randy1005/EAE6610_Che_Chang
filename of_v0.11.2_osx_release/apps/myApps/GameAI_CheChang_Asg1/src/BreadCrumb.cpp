993fad2986dd99a36977d8ee86efbfc171b2f443
RandyChang <randychang.10@gmail.com>
2022-02-05 12:55:08 -0700

    Pushed libs/openFrameworks folder

---
+++ of_v0.11.2_osx_release/apps/myApps/GameAI_CheChang_Asg1/src/BreadCrumb.cpp
@@ -0,0 +1,8 @@
+//
+//  BreadCrumb.cpp
+//  GameAI_CheChang_Asg1
+//
+//  Created by 張哲 on 2/5/22.
+//
+
+#include "BreadCrumb.hpp"
---
+++ of_v0.11.2_osx_release/apps/myApps/GameAI_CheChang_Asg1/src/BreadCrumb.hpp
@@ -0,0 +1,13 @@
+//
+//  BreadCrumb.hpp
+//  GameAI_CheChang_Asg1
+//
+//  Created by 張哲 on 2/5/22.
+//
+
+#ifndef BreadCrumb_hpp
+#define BreadCrumb_hpp
+
+#include <stdio.h>
+
+#endif /* BreadCrumb_hpp */
